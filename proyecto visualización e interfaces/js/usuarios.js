65b3938a6bcffa50b1c28b281f37aeb0bf8db266
Juanpi134 <juanpitocino_15@hotmail.com>
2025-11-11 18:44:45 -0300

    nuevos cambios

---
+++ proyecto visualización e interfaces/js/usuarios.js
@@ -0,0 +1,24 @@
+// Simulación de base de datos de usuarios (afiliados)
+const usuarios = [
+  {
+    nombre: "Juan Pérez",
+    dni: "12345678",
+    email: "juanperez@mail.com",
+    password: "1234",
+    plan: "Plan Esencial"
+  },
+  {
+    nombre: "María López",
+    dni: "87654321",
+    email: "marialopez@mail.com",
+    password: "abcd",
+    plan: "Plan Plus"
+  },
+  {
+    nombre: "Carlos Gómez",
+    dni: "22334455",
+    email: "carlosg@mail.com",
+    password: "pass2024",
+    plan: "Plan Premium"
+  }
+];
